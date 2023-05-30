b714a00611137e921b35999469bacaaba6b56798
Zoren Labrador <zorenzal@gmail.com>
2023-05-30 15:57:34 -0700

    Files

---
+++ index.js
@@ -0,0 +1,18 @@
+const mobileMenu = document.querySelector(".mobile--menu");
+const navMenu = document.querySelector(".nav--menu");
+const nav = document.querySelector("#nav");
+
+mobileMenu.addEventListener("click", () => {
+  mobileMenu.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+document.querySelectorAll(".nav--link").forEach(e =>
+  e.addEventListener("click", () => {
+    mobileMenu.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
+
+window.addEventListener("scroll", () => {
+  nav.classList.toggle("sticky", window.scrollY > 50);
+});
